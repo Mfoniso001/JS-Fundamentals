4131f010c38bb953051b82ce37e21fe5a77500a4
Mfoniso George <constancelestine16@gmail.com>
2025-05-23 21:47:28 +0100

    Fix: use console.log in 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,8 +1,7 @@
 const arg = process.argv[2];
 
 if (arg === undefined) {
-  console.log("No argument");
+  console.log('No argument');
 } else {
   console.log(arg);
 }
-37ae8c2 (Add 3-value_argument.js)
