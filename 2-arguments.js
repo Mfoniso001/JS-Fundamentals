1f0ddc21777212caba3fafc1037ab9594ef3275c
Mfoniso George <constancelestine16@gmail.com>
2025-05-23 21:10:03 +0100

    Add 3-value_argument.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,5 +1,6 @@
 const args = process.argv.slice(2);
 const count = args.length;
+<<<<<<< HEAD
 if (count === 0) {
   console.log("No argument");
 } else if (count === 1) {
@@ -7,3 +8,6 @@ if (count === 0) {
 } else {
   console.log("Arguments found");
 }
+=======
+if (count === 0) {console.log("No argument");} else if (count ===1) {console.log("Argument found");} else {console.log("Argument found");}
+>>>>>>> 37ae8c2 (Add 3-value_argument.js)
--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,1 +1,8 @@
+const arg = process.argv[2];
 
+if (arg === undefined) {
+  console.log("No argument");
+} else {
+  console.log(arg);
+}
+37ae8c2 (Add 3-value_argument.js)
