88dd907f6fc11bb08f76787df90aef5f098552f4
Mfoniso George <constancecelestine16@gmail.com>
2025-05-23 19:55:06 +0100

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
 No argument 
 Argument found
-=======
 const args = process.argv.slice(2);
 if (args.length === 0) {console.log("No argument");} else if (args.length ===1) {console.log("Argument found");} else {console.log("Argument found");}
->>>>>>> 70f3561 (Initial commit - added 2-arguments.js)
->>>>>>> 75f5dc0 (Initial commit - added 2-arguments.js)
+70f3561 (Initial commit - added 2-arguments.js)
+75f5dc0 (Initial commit - added 2-arguments.js)
