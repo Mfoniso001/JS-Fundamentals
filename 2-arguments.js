b8e0689f2079b226c5deb6a922c5a1e8ff7c0061
Mfoniso George <constancelestine16@gmail.com>
2025-05-23 21:23:15 +0100

    Save local changes before rebase

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,6 +1,5 @@
 const args = process.argv.slice(2);
 const count = args.length;
-<<<<<<< HEAD
 if (count === 0) {
   console.log("No argument");
 } else if (count === 1) {
@@ -8,6 +7,7 @@ if (count === 0) {
 } else {
   console.log("Arguments found");
 }
-=======
 if (count === 0) {console.log("No argument");} else if (count ===1) {console.log("Argument found");} else {console.log("Argument found");}
->>>>>>> 37ae8c2 (Add 3-value_argument.js)
+ 37ae8c2 (Add 3-value_argument.js)
+if (count === 0) {console.log("No argument");} else if (count ===1 ) {console.log("Argument found");} else {console.log("Arguments found");}
+b92f94e (Save local changes before rebase)
